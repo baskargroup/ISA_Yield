713b0582239f95ff72b60addc4de01eac1d5dc0a
Rudy <aapowadi@iastate.edu>
2025-09-03 14:35:24 -0500

    DEV: 1. Added county level data documentation. 2. Added slurm script for finetuning terramind for multimodal yield prediction using time-series data. 3. Updated config file to use CosineAnnealing LR_scheduler.

---
+++ county_level_data.md
@@ -0,0 +1,4 @@
+Link to the county level data USDA website (https://webapp.rma.usda.gov/apps/RIRS/RMACountyYieldsReport.aspx)
+
+1. Select the states, years, and commodities you like to download the county level data for (Corn, Soybean in our case). 
+2. Download the report. 
---
+++ nova_finetune.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # 36 processor core(s) per node 
+#SBATCH --mem=369G   # maximum memory per node
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=nova    # gpu node(s)
+#SBATCH --account=mech-ai
+#SBATCH --job-name="TerraFinetune"
+#SBATCH --mail-user=aapowadi@iastate.edu   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --output="Finetune%j.out" # job standard output file (%j replaced by job id)
+#SBATCH --error="Finetune%j.err" # job standard error file (%j replaced by job id)
+
+# SBATCH --cpus-per-task=16   # spread out to use 1 core per numa, set to 64 if tasks is 1
+# Environment setup
+# module purge
+# module load cuda/12.1
+# module load python/3.10
+
+echo "Job is starting on `hostname`"
+terratorch fit -c regression_config_tseries.yaml
